4423a1e6f7af33b795e778791c142a80e7fb636d
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-01-23 16:36:30 +0100

    Routing pages for project also added rout in case of URL input that doesnt exist

--- movie-search-engine/src/App.jsx
+++ movie-search-engine/src/App.jsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="home" element={<Home />} />
+          <Route path="favourites" element={<Favourites />} />
+        </Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
