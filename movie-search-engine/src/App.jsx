88940cb835cee6ba0591bf17528bf58119143c8e
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-01-23 16:49:57 +0100

    Imported componants

--- movie-search-engine/src/App.jsx
+++ movie-search-engine/src/App.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Layout from "./Layout";
+import Home from "./pages/Home";
+import Favourites from "./pages/Favourites";
+import NotFound from "./NotFound";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
+          <Route index element={<p>Welcome and enjoy our moives!</p>} />
           <Route path="home" element={<Home />} />
           <Route path="favourites" element={<Favourites />} />
         </Route>
