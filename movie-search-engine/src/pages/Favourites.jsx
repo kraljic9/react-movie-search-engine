1ac56b7e805b58b3005371d39e76661c2a9276c1
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-01-26 22:48:43 +0100

    Added favourites state from useContext

--- movie-search-engine/src/pages/Favourites.jsx
+++ movie-search-engine/src/pages/Favourites.jsx
@@ -1,4 +1,11 @@
+import { useContext } from "react";
+import { FavouriteContext } from "../FavouriteContext";
+
 function Favourites() {
+  const { favouritesList, setFavourites } = useContext(FavouriteContext);
+
+  console.log(favouritesList);
+
   return (
     <>
       <h1>Favourites page</h1>
