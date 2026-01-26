9f68bcb31c58a5466760831d3d5b502b6cc327cd
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-01-26 12:11:01 +0100

    Eddited movie image src

--- movie-search-engine/src/componants/MovieCard.jsx
+++ movie-search-engine/src/componants/MovieCard.jsx
@@ -1,13 +1,17 @@
 function MovieCard({ movie }) {
   return (
     <div className="movie-container">
-      <img src="" alt="" className="movie-img" />
+      <img
+        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+        alt=""
+        className="movie-img"
+      />
       <div className="movie-wrapper">
         <button className="addToFavourites-btn">♡</button>
       </div>
       <div className="movie-info">
         <p className="movie-title">{movie.title}</p>
-        <p className="movie-year">{movie.year}</p>
+        <p className="movie-year">{movie.release_date}</p>
       </div>
     </div>
   );
