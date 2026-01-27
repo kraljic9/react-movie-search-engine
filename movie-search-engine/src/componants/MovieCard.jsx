b8b961b96b6f222e895081f4ff7dfaf5730a5dc6
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-01-27 15:01:36 +0100

    Added Link wrap to the componant

--- movie-search-engine/src/componants/MovieCard.jsx
+++ movie-search-engine/src/componants/MovieCard.jsx
@@ -1,5 +1,6 @@
 import { useContext, useState } from "react";
 import { FavouriteContext } from "../FavouriteContext";
+import { Link } from "react-router-dom";
 
 function MovieCard({ movie }) {
   const [isFavourite, setIsFavourite] = useState(false);
@@ -7,17 +8,21 @@ function MovieCard({ movie }) {
 
   return (
     <div className="movie-container">
-      <img
-        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-        alt=""
-        className="movie-img"
-      />
+      <Link to={`/movies/${movie.id}`} className="Movie-link">
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt=""
+          className="movie-img"
+        />
+
+        <div className="movie-info">
+          <p className="movie-title">{movie.title}</p>
+          <p className="movie-year">{movie.release_date}</p>
+        </div>
+      </Link>
+
       <div className="movie-wrapper">
         <button className="addToFavourites-btn">♡</button>
-      </div>
-      <div className="movie-info">
-        <p className="movie-title">{movie.title}</p>
-        <p className="movie-year">{movie.release_date}</p>
       </div>
     </div>
   );
